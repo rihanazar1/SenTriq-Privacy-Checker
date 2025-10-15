b5a3b45bae5fff09e9c21fea4cd34528b426083f
rihanazar1 <rihanazhar5@gmail.com>
2025-10-15 19:09:42 +0530

    sentriq-first-commit

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,36 @@
+import logo from "./images/SentriqLogo.png";
+import img1 from "./images/img-1.png";
+import img2 from "./images/img-2.png"
+import img3 from "./images/img-3.png"
+import img4 from "./images/img-4.png"
+import img5 from "./images/img-5.png"
+import img6 from "./images/img-6.png"
+import img7 from "./images/img-7.png"
+import img8 from "./images/img-8.webp"
+import img9 from "./images/img-9.png"
+
+// export {
+//     logo,
+//     img1,
+//     img2,
+//     img3,
+//     img4,
+//     img5,
+//     img6,
+//     img7,
+//     img8,
+//     img9,
+// };
+
+export default {
+    logo,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+};
