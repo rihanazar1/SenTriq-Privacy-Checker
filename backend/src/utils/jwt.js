586a7300938a133bf813d4114413176eefe4c41f
rihanazar1 <rihanazhar5@gmail.com>
2025-10-22 17:38:36 +0530

    update refresh token

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -20,7 +20,7 @@ const generateRefreshToken = (user) => {
   };
 
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '14d',
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '14d',
   });
 };
 
